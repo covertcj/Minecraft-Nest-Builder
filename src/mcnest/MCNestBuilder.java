bf09aa9791444bf747bd6c20b2eb774aa110cb59
Christopher J. Covert <covertcj@rose-hulman.edu>
2011-01-30 23:52:03 +0000

    All loading works. We now just need to convert it to a 3D array, and implement saving. (Stuff to do is marked by TODO's).

--- src/mcnest/MCNestBuilder.java
+++ src/mcnest/MCNestBuilder.java
@@ -1,7 +1,5 @@
 package mcnest;
 
-import java.io.FileNotFoundException;
-
 public class MCNestBuilder
 {
 	public static void main(String[] args) {
@@ -9,12 +7,6 @@ public class MCNestBuilder
 	}
 	
 	MCNestBuilder(String[] args) {
-		MinecraftWorld world = new MinecraftWorld("/home/covertcj/.minecraft/saves/", "World1");
-		
-		try {
-			world.LoadChunk(0, 0);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
+		MinecraftWorld world = new MinecraftWorld("C:/Users/covertcj/AppData/Roaming/.minecraft/saves/", "World1");
 	}
 }
